6c16199e4682c9af76ff6e1d5968b66dd91a0a75
Zyphery <58096190+Zyphery@users.noreply.github.com>
2020-03-31 20:44:31 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "ZCEngine.h"
+#include <stdlib.h> 
+
+using namespace ZCPP;
+
+class Example : public ZCEngine
+{
+public:
+	Example()
+	{
+
+	}
+
+public:
+	bool Start() override
+	{
+		FrameRate = -1; // Set to run at infinite fps
+		return true;
+	}
+
+	bool Update(float DeltaTime) override
+	{
+		for(int y = 0; y < ScreenHeight(); y++) {
+			for(int x = 0; x < ScreenWidth(); x++) {
+				std::string a;
+				for(int i = 0; i < TextLimit(); i++)
+				{
+					std::string b(1,(char)(rand() % 31 - (128 - 31)));
+					a += b;
+				}
+				Draw(x,y,stoc(a));
+			}
+    }
+		DrawScreen(); // Must be used to draw screen
+		return true;
+	}
+
+	bool Destroy() override
+	{
+		return true;
+	}
+};
+
+int main()
+{
+	Example Main;
+	if(Main.Build(30,20,2))
+		Main.Initiate();
+}
