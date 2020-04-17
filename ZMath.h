c958308442bc7c8e145cfa2ba19aa5f30ffdc7ff
Zyphery <58096190+Zyphery@users.noreply.github.com>
2020-04-16 23:43:57 -0700

    Updated ZMath
    - Fixed functions
        - Distance function was incorrect (i'm stupid)
    - Added comparing operators for all vectors
    - Added more math functions

--- ZMath.h
+++ ZMath.h
@@ -1,3 +1,5 @@
+#pragma once
+#include <iostream>
 #include <cmath>
 
 // This math  library has no effect  on the main  ZCEngine  file.
@@ -7,134 +9,205 @@
 // math  functions  such  as  adding,  subtracting,  multiplying,
 // dividing,  and  normalizing.  The  add and subtract  functions
 // only work on  2 vectors.  The multiply  and divide  can use  a
-// vector or float.  The normalize  function is  called by  using
-// ~A;  A  as the  vector type.  Normalize is a void  and doesn't
-// return anything. More detail can be found inside the namespace
+// vector or float. More detail can be found inside the namespace
 
 namespace ZCPP
 {
+	// Vector classes:
+
 	class Vector2D // Vector2D class
 	{
+	public:
 		float x, y;
 
-		Vector2D() {x = 0; y = 0;}
+		Vector2D() { x = 0; y = 0; }
+		Vector2D(float _) { x = _; y = _; }
 		Vector2D(float _x, float _y) { x = _x; y = _y; }
 
-		static float Length(Vector2D A) { return sqrt(A.x*A.x + A.y*A.y); }
-		static Vector2D Normalize(Vector2D A) { float L = Length(A); return Vector2D(A.x/L, A.y/L); }
+		static float Length(Vector2D A) { return sqrt(A.x * A.x + A.y * A.y); }
+		static float Distance(Vector2D A, Vector2D B) { return sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2)); }
+		static Vector2D Normalize(Vector2D A) { float L = Length(A); return Vector2D(A.x / L, A.y / L); }
 
-		static std::string ToString(Vector2D A) { return "<" + std::to_string(A.x)+", "+std::to_string(A.y)+">"; }
-		std::string ToString() { return "< " + std::to_string(this->x)+", "+std::to_string(this->y)+" >"; }
+		static std::string ToString(Vector2D A) { return "<" + std::to_string(A.x) + ", " + std::to_string(A.y) + ">"; }
+		std::string ToString() { return "< " + std::to_string(this->x) + ", " + std::to_string(this->y) + " >"; }
+
+		static Vector2D Max(Vector2D A, Vector2D B)
+		{
+			Vector2D C;
+			C.x = std::fmax(A.x, B.x);
+			C.y = std::fmax(A.y, B.y);
+			return C;
+		}
+
+		static Vector2D Min(Vector2D A, Vector2D B)
+		{
+			Vector2D C;
+			C.x = std::fmin(A.x, B.x);
+			C.y = std::fmin(A.y, B.y);
+			return C;
+		}
 	};
 
-	Vector2D operator * (Vector2D const& A, float const& B) {return Vector2D(A.x * B, A.y * B);}
-	Vector2D operator * (float const& B, Vector2D const& A) {return Vector2D(A.x * B, A.y * B);}
-	// Multiply Vector2D by float
-	// Multiplies each float inside Vector2D A by float B
+	bool operator == (const Vector2D& A, const Vector2D& B) { return (A.x == B.x && A.y == B.y); }
+	bool operator < (const Vector2D& A, const Vector2D& B) { return (A.x < B.x && A.y < B.y); }
+	bool operator > (const Vector2D& A, const Vector2D& B) { return (A.x > B.x && A.y > B.y); }
+	bool operator <= (const Vector2D& A, const Vector2D& B) { return (A.x <= B.x && A.y <= B.y); }
+	bool operator >= (const Vector2D& A, const Vector2D& B) { return (A.x >= B.x && A.y >= B.y); }
+	bool operator != (const Vector2D& A, const Vector2D& B) { return (A.x != B.x && A.y != B.y); }
 
-	Vector2D operator * (Vector2D const& A, Vector2D const& B) {return Vector2D(A.x * B.x, A.y * B.y);}
-	// Multiply Vector2D by Vector2D
-	// Multiplies each same variable: X * X; Y * Y;
+	Vector2D operator + (const Vector2D& A, const Vector2D& B) { return Vector2D(A.x + B.x, A.y + B.y); }
+	// Add a Vector2D to a Vector2D
 
-	Vector2D operator / (Vector2D const& A, float const& B) {return Vector2D(A.x / B, A.y / B);}
-	Vector2D operator / (float const& B, Vector2D const& A) {return Vector2D(A.x / B, A.y / B);}
-	// Divide Vector2D by float
-	// Divides each float inside Vector2D A by float B
+	Vector2D operator - (const Vector2D& A, const Vector2D& B) { return Vector2D(A.x - B.x, A.y - B.y); }
+	// Subtract a Vector2D by a Vector2D
 
-	Vector2D operator / (Vector2D const& A, Vector2D const& B) {return Vector2D(A.x / B.x, A.y / B.y);}
-	// Divide Vector2D by Vector2D
-	// Divides each same variable: X / X; Y / Y;
+	Vector2D operator * (const Vector2D& A, const Vector2D& B) { return Vector2D(A.x * B.x, A.y * B.y); }
+	// Multiply a Vector2D by a Vector2D
 
-	Vector2D operator + (Vector2D const& A, Vector2D const& B) {return Vector2D(A.x + B.x, A.y + B.y);}
-	// Add Vector2D by Vector2D
-	// Adds each same variable: X + X; Y + Y;
+	Vector2D operator * (const Vector2D& A, const float& B) { return Vector2D(A.x * B, A.y * B); }
+	Vector2D operator * (const float& A, const Vector2D& B) { return(B * A); }
+	// Multiply a Vector2D by a float
 
-	Vector2D operator - (Vector2D const& A, Vector2D const& B) {return Vector2D(A.x - B.x, A.y - B.y);}
-	// Subtract Vector2D by Vector2D
-	// Subtracts each same variable: X - X; Y - Y;
+	Vector2D operator / (const Vector2D& A, const Vector2D& B) { return Vector2D(A.x / B.x, A.y / B.y); }
+	// Divide a Vector2D by a Vector2D
 
-	void operator ~ (Vector2D &A) { A = Vector2D::Normalize(A);}
+	Vector2D operator / (const Vector2D& A, const float& B) { return Vector2D(A.x / B, A.y / B); }
+	Vector2D operator / (const float& A, const Vector2D& B) { return (B / A); }
+	// Divide a Vector2D by a float
+
+	Vector2D& operator += (Vector2D& A, const Vector2D& B) { A.x += B.x; A.y += B.y; return A; }
+	Vector2D& operator -= (Vector2D& A, const Vector2D& B) { A.x -= B.x; A.y -= B.y; return A; }
+	Vector2D& operator *= (Vector2D& A, const float& B) { A.x *= B; A.y *= B; return A; }
+	Vector2D& operator /= (Vector2D& A, const float& B) { A.x /= B; A.y /= B; return A; }
+
+	Vector2D operator ~ (Vector2D const& A) { return Vector2D::Normalize(A); }
 	// Normalizes Vector2D A
-	// Sets A as normalized upon call
+
+	Vector2D operator ! (Vector2D& const A) { return Vector2D(abs(A.x), abs(A.y)); }
+	// absolute value of Vector2D A
+
+	float DP2D(Vector2D A, Vector2D B) { Vector2D C = A * B; return C.x + C.y; }
 
 
 
 	class Vector3D // Vector3D class
 	{
+	public:
 		float x, y, z;
 
-		Vector3D() {x = 0; y = 0; z = 0;}
+		Vector3D() { x = 0; y = 0; z = 0; }
+		Vector3D(float _) { x = _; y = _; z = _; }
 		Vector3D(float _x, float _y, float _z) { x = _x; y = _y; z = _z; }
 
-		static float Length(Vector3D A) { return sqrt(A.x*A.x + A.y*A.y + A.z*A.z); }
-		static Vector3D Normalize(Vector3D A) { float L = Length(A); return Vector3D(A.x/L, A.y/L, A.z/L); }
+		static float Length(Vector3D A) { return sqrt(A.x * A.x + A.y * A.y + A.z * A.z); }
+		static float Distance(Vector3D A, Vector3D B) { return sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2) + pow(B.z - A.z, 2)); }
+		static Vector3D Normalize(Vector3D A) { float L = Length(A); return Vector3D(A.x / L, A.y / L, A.z / L); }
 
-		static std::string ToString(Vector3D A) { return "<" + std::to_string(A.x)+", "+std::to_string(A.y)+", "+std::to_string(A.z)+">"; }
-		std::string ToString() { return "< " + std::to_string(this->x)+", "+std::to_string(this->y)+", "+std::to_string(this->z)+" >"; }
+		static std::string ToString(Vector3D A) { return "<" + std::to_string(A.x) + ", " + std::to_string(A.y) + ", " + std::to_string(A.z) + ">"; }
+		std::string ToString() { return "< " + std::to_string(this->x) + ", " + std::to_string(this->y) + ", " + std::to_string(this->z) + " >"; }
+
+		static Vector3D Max(Vector3D A, Vector3D B)
+		{
+			Vector3D C;
+			C.x = std::fmax(A.x, B.x);
+			C.y = std::fmax(A.y, B.y);
+			C.z = std::fmax(A.z, B.z);
+			return C;
+		}
+
+		static Vector3D Min(Vector3D A, Vector3D B)
+		{
+			Vector3D C;
+			C.x = std::fmin(A.x, B.x);
+			C.y = std::fmin(A.y, B.y);
+			C.z = std::fmin(A.z, B.z);
+			return C;
+		}
 	};
 
-	Vector3D operator * (Vector3D const& A, float const& B) {return Vector3D(A.x * B, A.y * B, A.z * B);}
-	Vector3D operator * (float const& B, Vector3D const& A) {return Vector3D(A.x * B, A.y * B, A.z * B);}
-	// Multiply Vector3D by float
-	// Multiplies each float inside Vector3D A by float B
+	bool operator == (const Vector3D& A, const Vector3D& B) { return (A.x == B.x && A.y == B.y && A.z == B.z); }
+	bool operator < (const Vector3D& A, const Vector3D& B) { return (A.x < B.x && A.y < B.y && A.z < B.z); }
+	bool operator > (const Vector3D& A, const Vector3D& B) { return (A.x > B.x && A.y > B.y && A.z > B.z); }
+	bool operator <= (const Vector3D& A, const Vector3D& B) { return (A.x <= B.x && A.y <= B.y && A.x <= B.x); }
+	bool operator >= (const Vector3D& A, const Vector3D& B) { return (A.x >= B.x && A.y >= B.y && A.x >= B.x); }
+	bool operator != (const Vector3D& A, const Vector3D& B) { return (A.x != B.x && A.y != B.y && A.x != B.x); }
 
-	Vector3D operator * (Vector3D const& A, Vector3D const& B) {return Vector3D(A.x * B.x, A.y * B.y, A.z * B.z);}
-	// Multiply Vector3D by Vector3D
-	// Multiplies each same variable: X * X; Y * Y; Z * Z;
+	Vector3D operator + (const Vector3D& A, const Vector3D& B) { return Vector3D(A.x + B.x, A.y + B.y, A.z + B.z); }
+	// Add a Vector3D to a Vector3D
 
-	Vector3D operator / (Vector3D const& A, float const& B) {return Vector3D(A.x / B, A.y / B, A.z / B);}
-	Vector3D operator / (float const& B, Vector3D const& A) {return Vector3D(A.x / B, A.y / B, A.z / B);}
-	// Divide Vector3D by float
-	// Divides each float inside Vector3D A by float B
+	Vector3D operator - (const Vector3D& A, const Vector3D& B) { return Vector3D(A.x - B.x, A.y - B.y, A.z - B.z); }
+	// Subtract a Vector3D by a Vector3D
 
-	Vector3D operator / (Vector3D const& A, Vector3D const& B) {return Vector3D(A.x / B.x, A.y / B.y, A.z / B.z);}
-	// Divide Vector3D by Vector3D
-	// Divides each same variable: X / X; Y / Y; Z / Z;
+	Vector3D operator * (const Vector3D& A, const Vector3D& B) { return Vector3D(A.x * B.x, A.y * B.y, A.z * B.z); }
+	// Multiply a Vector3D by a Vector3D
 
-	Vector3D operator + (Vector3D const& A, Vector3D const& B) {return Vector3D(A.x + B.x, A.y + B.y, A.z + B.z);}
-	// Add Vector3D by Vector3D
-	// Adds each same variable: X + X; Y + Y; Z + Z;
+	Vector3D operator * (const Vector3D& A, const float& B) { return Vector3D(A.x * B, A.y * B, A.z * B); }
+	Vector3D operator * (const float& A, const Vector3D& B) { return(B * A); }
+	// Multiply a Vector3D by a float
 
-	Vector3D operator - (Vector3D const& A, Vector3D const& B) {return Vector3D(A.x - B.x, A.y - B.y, A.z - B.z);}
-	// Subtract Vector3D by Vector3D
-	// Subtracts each same variable: X - X; Y - Y; Z - Z;
+	Vector3D operator / (const Vector3D& A, const Vector3D& B) { return Vector3D(A.x / B.x, A.y / B.y, A.z / B.z); }
+	// Divide a Vector3D by a Vector3D
 
-	void operator ~ (Vector3D &A) { A = Vector3D::Normalize(A);}
+	Vector3D operator / (const Vector3D& A, const float& B) { return Vector3D(A.x / B, A.y / B, A.z / B); }
+	Vector3D operator / (const float& A, const Vector3D& B) { return (B / A); }
+	// Divide a Vector3D by a float
+
+	Vector3D& operator += (Vector3D& A, const Vector3D& B) { A.x += B.x; A.y += B.y; A.z += B.z; return A; }
+	Vector3D& operator -= (Vector3D& A, const Vector3D& B) { A.x -= B.x; A.y -= B.y; A.z += B.z; return A; }
+	Vector3D& operator *= (Vector3D& A, const float& B) { A.x *= B; A.y *= B; A.z *= B; return A; }
+	Vector3D& operator /= (Vector3D& A, const float& B) { A.x /= B; A.y /= B; A.z /= B; return A; }
+
+	Vector3D operator ~ (Vector3D& A) { return Vector3D::Normalize(A); }
 	// Normalizes Vector3D A
-	// Sets A as normalized upon call
 
-	float DP(Vector3D A, Vector3D B) { Vector3D C = A * B; return C.x+C.y+C.z; }
-	Vector3D CP(Vector3D A, Vector3D B) { Vector3D C; C.x = A.y * B.z - A.z * B.y; C.y = A.z * B.x - A.x * B.z; C.z = A.x * B.y - A.y * B.x; return C; }
+	Vector3D operator ! (Vector3D& A) { return Vector3D(abs(A.x), abs(A.y), abs(A.z)); }
+	// absolute value of Vector3D A
+	
+	float DP3D(Vector3D A, Vector3D B) { Vector3D C = A * B; return C.x + C.y + C.z; }
+	Vector3D CP3D(Vector3D A, Vector3D B) { Vector3D C; C.x = A.y * B.z - A.z * B.y; C.y = A.z * B.x - A.x * B.z; C.z = A.x * B.y - A.y * B.x; return C; }
+
+
 
 	// Still figuring out Quaternions, use at your own risk
 	class Quaternion // Quaternion class
 	{
+	public:
 		float w, x, y, z;
 
 		Quaternion() { w = 1; x = 0; y = 0; z = 0; }
-		Quaternion(float _w, float _x, float _y, float _z) { w = _w; x = _x; y = _y; z =_z; }
+		Quaternion(float _) { w = _; x = _; y = _; z = _; }
+		Quaternion(float _w, float _x, float _y, float _z) { w = _w; x = _x; y = _y; z = _z; }
 
-		static float Length(Quaternion A) { return sqrt(A.w*A.w + A.x*A.x + A.y*A.y + A.z*A.z); }
-		static Quaternion Normalize(Quaternion A) { float L = Length(A); return Quaternion(A.w/L, A.x/L, A.y/L, A.z/L);}
+		static float Length(Quaternion A) { return sqrt(A.w * A.w + A.x * A.x + A.y * A.y + A.z * A.z); }
+		static float Distance(Quaternion A, Quaternion B) { return sqrt(pow(B.w - A.w, 2) + pow(B.x - A.x, 2) + pow(B.y - A.y, 2) + pow(B.z - A.z, 2)); }
+		static Quaternion Normalize(Quaternion A) { float L = Length(A); return Quaternion(A.w / L, A.x / L, A.y / L, A.z / L); }
 
-		static std::string ToString(Quaternion A) { return "<" + std::to_string(A.w)+", "+std::to_string(A.x)+", "+std::to_string(A.y)+", "+std::to_string(A.z)+">"; }
-		std::string ToString() { return "< " + std::to_string(this->w)+", "+std::to_string(this->x)+", "+std::to_string(this->y)+", "+std::to_string(this->z)+" >"; }
+		static std::string ToString(Quaternion A) { return "<" + std::to_string(A.w) + ", " + std::to_string(A.x) + ", " + std::to_string(A.y) + ", " + std::to_string(A.z) + ">"; }
+		std::string ToString() { return "< " + std::to_string(this->w) + ", " + std::to_string(this->x) + ", " + std::to_string(this->y) + ", " + std::to_string(this->z) + " >"; }
 	};
 
+	bool operator == (const Quaternion& A, const Quaternion& B) { return (A.w == B.w && A.x == B.x && A.y == B.y && A.z == B.z); }
+	bool operator < (const Quaternion& A, const Quaternion& B) { return (A.w < B.w && A.x < B.x && A.y < B.y && A.z < B.z); }
+	bool operator > (const Quaternion& A, const Quaternion& B) { return (A.w > B.w && A.x > B.x&& A.y > B.y&& A.z > B.z); }
+	bool operator <= (const Quaternion& A, const Quaternion& B) { return (A.w <= B.w && A.x <= B.x && A.y <= B.y && A.x <= B.x); }
+	bool operator >= (const Quaternion& A, const Quaternion& B) { return (A.w >= B.w && A.x >= B.x && A.y >= B.y && A.x >= B.x); }
+	bool operator != (const Quaternion& A, const Quaternion& B) { return (A.w != B.w && A.x != B.x && A.y != B.y && A.x != B.x); }
 
-	void operator - (Quaternion &A) { A.w = -A.w; A.x = -A.x; A.y = -A.y; A.z = -A.z; }
+	Quaternion operator - (Quaternion& A) { return Quaternion(-A.w, -A.x, -A.y, -A.z); }
 	// Turns the given Quaternion into its Conjugate
 
-	void operator ~ (Quaternion &A) { A = Quaternion::Normalize(A);}
-	// Normalizes Vector A
-	// Sets A as normalized upon call
+	Quaternion operator ~ (Quaternion& A) { return Quaternion::Normalize(A); }
+	// Normalizes Vector3D A
 
-	Quaternion operator * (Quaternion const& A, Quaternion const& B) {Quaternion b = Quaternion(A.x * B.w + A.y * B.z - A.z * B.y + A.w * B.x, -A.x * B.z + A.y * B.w + A.z * B.x + A.w * B.y, A.x * B.y - A.y * B.x + A.z * B.w + A.w * B.z, -A.x * B.x - A.y * B.y - A.z * B.z + A.w * B.w); ~b; return b;}
+	Quaternion operator * (Quaternion const& A, Quaternion const& B) { Quaternion b = Quaternion(A.x * B.w + A.y * B.z - A.z * B.y + A.w * B.x, -A.x * B.z + A.y * B.w + A.z * B.x + A.w * B.y, A.x * B.y - A.y * B.x + A.z * B.w + A.w * B.z, -A.x * B.x - A.y * B.y - A.z * B.z + A.w * B.w); ~b; return b; }
 	// Multiplies a Quaternion by a Quaternion
 
-	Quaternion RotationQuaternion(float r, Vector A) { Quaternion q; q.w = r; q.x = A.x; q.y = A.y; q.z = A.z; ~q; return q; }
-	// Creates a rotation Quaternion using r (radians) and A (Vector Axis)
+	Quaternion RotationQuaternion(float r, Vector3D A) { Quaternion q; q.w = r; q.x = A.x; q.y = A.y; q.z = A.z; ~q; return q; }
+	// Creates a rotation Quaternion using r (radians) and A (Vector3D Axis)
 
-	Vector QuaternionRotate(float r, Vector V, Vector A) { Quaternion q = RotationQuaternion(r, A); Vector u = Vector(q.x, q.y, q.z); return 2 * DP(u, V) * u + (pow(q.w,2) - DP(u, u)) * V + 2 * q.w * CP(u, V); }
-	// Rotates V (Vector) around A (axis) by r (radians) using a Rotation Quaternion created with r & A
+	Vector3D QuaternionRotate(float r, Vector3D V, Vector3D A) { Quaternion q = RotationQuaternion(r, A); Vector3D u = Vector3D(q.x, q.y, q.z); return 2 * DP3D(u, V) * u + (pow(q.w, 2) - DP3D(u, u)) * V + 2 * q.w * CP3D(u, V); }
+	// Rotates V (Vector3D) around A (axis) by r (radians) using a Rotation Quaternion created with r & A
+
+	Quaternion operator ! (Quaternion& A) { return Quaternion(abs(A.w), abs(A.x), abs(A.y), abs(A.z)); }
+	// absolute value of Vector3D A
 };
