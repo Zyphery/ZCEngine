0dc21c07cb45beb7e84e278640457636bfbb609d
Zyphery <58096190+Zyphery@users.noreply.github.com>
2020-05-09 16:00:08 -0700

    Fixed bugs, updated functions
    Added include of string
    Added degrees to radians and radians to degrees functions
    Added Absolute function
    Bug fixing on Quaternions - dont use yet

--- ZMath.h
+++ ZMath.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <string>
 #include <cmath>
 
 
@@ -14,7 +15,10 @@
 
 namespace ZCPP
 {
-	double const Pi = 3.14159265359;
+	const double Pi = 3.14159265359;
+
+	const float DegtoRad = Pi * 2. / 360.;
+	const float RadtoDeg = 1. / DegtoRad;
 
 	// Vector classes:
 
@@ -31,6 +35,9 @@ namespace ZCPP
 		float Length() { return Length(Vector2D(this->x,this->y)); }
 		static float Distance(Vector2D A, Vector2D B) { return sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2)); }
 		static Vector2D Normalize(Vector2D A) { float L = Length(A); return Vector2D(A.x / L, A.y / L); }
+		void Normalize() { Vector2D V = { this->x, this->y}; Vector2D VN = Normalize(V); this->x = VN.x; this->y = VN.y; }
+		static Vector2D Abs(Vector2D A) { return Vector2D(abs(A.x), abs(A.y)); }
+		void Abs() { Vector2D V = Abs(Vector2D(this->x, this->y)); this->x = V.x; this->y = V.y; }
 
 		static std::string ToString(Vector2D A) { return "<" + std::to_string(A.x) + ", " + std::to_string(A.y) + ">"; }
 		std::string ToString() { return "< " + std::to_string(this->x) + ", " + std::to_string(this->y) + " >"; }
@@ -87,7 +94,7 @@ namespace ZCPP
 	Vector2D operator ~ (Vector2D const& A) { return Vector2D::Normalize(A); }
 	// Normalizes Vector2D A
 
-	Vector2D operator ! (Vector2D& const A) { return Vector2D(abs(A.x), abs(A.y)); }
+	Vector2D operator ! (Vector2D& const A) { A.Abs();  return A; }
 	// absolute value of Vector2D A
 
 	float DP2D(Vector2D A, Vector2D B) { Vector2D C = A * B; return C.x + C.y; }
@@ -109,6 +116,9 @@ namespace ZCPP
 		float Length() { return Length(Vector3D(this->x, this->y, this->z)); }
 		static float Distance(Vector3D A, Vector3D B) { return sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2) + pow(B.z - A.z, 2)); }
 		static Vector3D Normalize(Vector3D A) { float L = Length(A); return Vector3D(A.x / L, A.y / L, A.z / L); }
+		void Normalize() { Vector3D V = { this->x, this->y, this->z }; Vector3D VN = Normalize(V); this->x = VN.x; this->y = VN.y; this->z = VN.z; }
+		static Vector3D Abs(Vector3D A) { return Vector3D(abs(A.x), abs(A.y), abs(A.z)); }
+		void Abs() { Vector3D V = Abs(Vector3D(this->x, this->y, this->z)); this->x = V.x; this->y = V.y; this->z = V.z; }
 
 		static std::string ToString(Vector3D A) { return "<" + std::to_string(A.x) + ", " + std::to_string(A.y) + ", " + std::to_string(A.z) + ">"; }
 		std::string ToString() { return "< " + std::to_string(this->x) + ", " + std::to_string(this->y) + ", " + std::to_string(this->z) + " >"; }
@@ -167,7 +177,7 @@ namespace ZCPP
 	Vector3D operator ~ (Vector3D& A) { return Vector3D::Normalize(A); }
 	// Normalizes Vector3D A
 
-	Vector3D operator ! (Vector3D& A) { return Vector3D(abs(A.x), abs(A.y), abs(A.z)); }
+	Vector3D operator ! (Vector3D& A) { A.Abs(); return A; }
 	// absolute value of Vector3D A
 	
 	float DP3D(Vector3D A, Vector3D B) { Vector3D C = A * B; return C.x + C.y + C.z; }
@@ -189,6 +199,9 @@ namespace ZCPP
 		float Length() { return Length(Quaternion(this->w, this->x, this->y, this->z)); }
 		static float Distance(Quaternion A, Quaternion B) { return sqrt(pow(B.w - A.w, 2) + pow(B.x - A.x, 2) + pow(B.y - A.y, 2) + pow(B.z - A.z, 2)); }
 		static Quaternion Normalize(Quaternion A) { float L = Length(A); return Quaternion(A.w / L, A.x / L, A.y / L, A.z / L); }
+		void Normalize() { Quaternion V = { this->w, this->x, this->y, this->z }; Quaternion QN = Normalize(V); this->w = QN.w; this->x = QN.x; this->y = QN.y; this->z = QN.z; }
+		static Quaternion Abs(Quaternion A) { return Quaternion(abs(A.w), abs(A.x), abs(A.y), abs(A.z)); }
+		void Abs() { Quaternion Q = Abs(Quaternion(this->w, this->x, this->y, this->z)); this->w = Q.w;  this->x = Q.x; this->y = Q.y; this->z = Q.z; }
 
 		static std::string ToString(Quaternion A) { return "<" + std::to_string(A.w) + ", " + std::to_string(A.x) + ", " + std::to_string(A.y) + ", " + std::to_string(A.z) + ">"; }
 		std::string ToString() { return "< " + std::to_string(this->w) + ", " + std::to_string(this->x) + ", " + std::to_string(this->y) + ", " + std::to_string(this->z) + " >"; }
@@ -201,21 +214,37 @@ namespace ZCPP
 	bool operator >= (const Quaternion& A, const Quaternion& B) { return (A.w >= B.w && A.x >= B.x && A.y >= B.y && A.x >= B.x); }
 	bool operator != (const Quaternion& A, const Quaternion& B) { return (A.w != B.w && A.x != B.x && A.y != B.y && A.x != B.x); }
 
-	Quaternion operator - (Quaternion& A) { return Quaternion(-A.w, -A.x, -A.y, -A.z); }
-	// Turns the given Quaternion into its Conjugate
-
 	Quaternion operator ~ (Quaternion& A) { return Quaternion::Normalize(A); }
 	// Normalizes Vector3D A
+	
+	Quaternion operator - (Quaternion& A) { Quaternion Q = Quaternion(-A.w, -A.x, -A.y, -A.z); Q = ~Q; return Q; }
+	// Turns the given Quaternion into its Conjugate
 
-	Quaternion operator * (Quaternion const& A, Quaternion const& B) { Quaternion b = Quaternion(A.x * B.w + A.y * B.z - A.z * B.y + A.w * B.x, -A.x * B.z + A.y * B.w + A.z * B.x + A.w * B.y, A.x * B.y - A.y * B.x + A.z * B.w + A.w * B.z, -A.x * B.x - A.y * B.y - A.z * B.z + A.w * B.w); ~b; return b; }
+	Quaternion operator * (Quaternion const& A, Quaternion const& B) { return Quaternion(A.x * B.w + A.y * B.z - A.z * B.y + A.w * B.x, -A.x * B.z + A.y * B.w + A.z * B.x + A.w * B.y, A.x * B.y - A.y * B.x + A.z * B.w + A.w * B.z, -A.x * B.x - A.y * B.y - A.z * B.z + A.w * B.w); }
 	// Multiplies a Quaternion by a Quaternion
 
-	Quaternion RotationQuaternion(float r, Vector3D A) { Quaternion q; q.w = r; q.x = A.x; q.y = A.y; q.z = A.z; ~q; return q; }
+	/* order of quaternion multiplication
+	A.w*B.w - A.x*B.x - A.y*B.y - A.z*B.z
+	A.w*B.x + A.x*B.w + A.y*B.z - A.z*B.y
+	A.w*B.y - A.x*B.z + A.y*B.w + A.z*B.x
+	A.w*B.z + A.x*B.y - A.y*B.x + A.z*B.w
+	*/
+
+	Quaternion RotationQuaternion(float r, Vector3D A) { Quaternion q = Quaternion(r, A.x, A.y, A.z); ~q; return q; }
 	// Creates a rotation Quaternion using r (radians) and A (Vector3D Axis)
 
-	Vector3D QuaternionRotate(float r, Vector3D V, Vector3D A) { Quaternion q = RotationQuaternion(r, A); Vector3D u = Vector3D(q.x, q.y, q.z); return 2 * DP3D(u, V) * u + (pow(q.w, 2) - DP3D(u, u)) * V + 2 * q.w * CP3D(u, V); }
-	// Rotates V (Vector3D) around A (axis) by r (radians) using a Rotation Quaternion created with r & A
+	Vector3D QuaternionRotate(float r, Vector3D V, Vector3D A)
+	{
+		Quaternion RQ = RotationQuaternion(r, A);		// Rotation Quaternion
+		Quaternion CQ = -RQ;							// Conjugate Quaternion
+		Quaternion VQ = Quaternion(0, V.x, V.y, V.z);	// Vector to Quaternion
+		RQ = RQ * VQ * CQ;	// Rotation Quaternion * Vector (quaternion form) * Conjugate of Rotation Quaternion
+		return Vector3D(RQ.x, RQ.y, RQ.z);
 
-	Quaternion operator ! (Quaternion& A) { return Quaternion(abs(A.w), abs(A.x), abs(A.y), abs(A.z)); }
+	}
+	//return 2 * DP3D(u, V) * u + (pow(q.w, 2) - DP3D(u, u)) * V + 2 * q.w * CP3D(u, V); }
+	// Rotates V (Vector3D) around A (axis) by r (radians) using a Rotation Quaternion created with r & A
+	
+	Quaternion operator ! (Quaternion& A) { A.Abs(); return A; }
 	// absolute value of Vector3D A
 };
